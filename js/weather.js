fd0f626621e04e1aeced2ce36dc9e36b1bfb0a0e
kris.choo <seokhunk@gmail.com>
2024-12-15 21:16:24 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -4,7 +4,7 @@ function onGeoOk(position) {
 	const lat = position.coords.latitude;
 	const lon = position.coords.longitude;
 	console.log("당신의 위치:", lat, lon);
-	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}1&appid=${API_KEY}&units=metric`;
+	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 	fetch(url)
 		.then((response) => response.json())
 		.then((data) => {
